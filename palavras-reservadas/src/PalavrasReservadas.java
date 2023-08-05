aa8456beb03c5230954a3f4cbc1bd1dbb3125c53
Iago <iagorcamargo@gmail.com>
2023-08-05 16:45:43 -0300

    Atualizando

---
+++ palavras-reservadas/src/PalavrasReservadas.java
@@ -0,0 +1,32 @@
+public class PalavrasReservadas {
+
+  public static void main(String[] args) {
+    System.out.print("Fui executado");
+  }
+}
+// CONTROLE DE PACOTES
+// import - importa pacotes ou classes para dentro do cód
+// package - especifica a que pacote todas as classes de um arquivo pertencem
+// MODIFICADORES DE ACESSO
+// public - acesso de qualquer classe
+// private - acesso apenas dentro da classe
+// protected - acesso por classes no mesmo pacote e subclasses
+// PRIMITIDOS
+/* boolean, byte, char, double, float, int, long, short, void(ausencia de valor, não espera retorno) */
+// MODIFICADORES DE CLASSES, VARIAVEIS OU METODOS
+// abstract - classe que não pode ser instanciada ou método que precisa ser implementado por uma subclasse não abstrata
+// class - especifica uma classe
+// extends - indica a superclasse que a subclasse esta estendendo
+// final - impossibilita que uma classe seja estendida, que um metodo seja sobrescrito ou que uma variavel seja reinicializada
+// implements - indica as interfaces que uma classe ira implementar
+// native - indica que um metodo esta escrito em uma linguagem dependendo de plataforma, como o C
+// new - instancia um novo objeto, cjamando seu construtor
+// static - faz um metodo ou variavel pertencer a classe ao inves de as instâncias
+// strctfp - usado em frente a um metodo ou classe para indicar que os numeros de ponto flutuante seguirão as regras em todas expressões
+// synchronized - indica um metodo que só pode ser acessado por uma thread de cada vez
+// transient - impede a serialização de campos
+// volatile - uma variavel pode ser alterada durando o uso de threads
+// CONTROLE DE FLUXO DENTRO DE UM BLOCO DE CÓDIGO
+// break - sai do bloco em que esta
+// case - executa o bloco dependendo do teste switch
+// continue - pula a execução do codigo que viria apos essa linha e vai para a prox passagem do loop
