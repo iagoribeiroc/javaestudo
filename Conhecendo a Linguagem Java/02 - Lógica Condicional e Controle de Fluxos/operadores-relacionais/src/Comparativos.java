aeb162910691beb9707c5fa456abde7f50fed391
Iago <iagorcamargo@gmail.com>
2023-08-05 17:41:32 -0300

    Att

---
+++ Conhecendo a Linguagem Java/02 - Lógica Condicional e Controle de Fluxos/operadores-relacionais/src/Comparativos.java
@@ -0,0 +1,16 @@
+public class Comparativos {
+
+  public static void main(String[] args) throws Exception {
+    int n1 = 5;
+    int n2 = 9;
+
+    String nome1 = "Iago";
+    String nome2 = "Gabrielle";
+
+    System.out.println("n1 = n2? " + (n1 == n2));
+    System.out.println("n1 > n2? " + (n1 > n2));
+    System.out.println("n1 < n2? " + (n1 < n2));
+    System.out.println("n1 != n2? " + (n1 != n2));
+    System.out.println("nome1 = nome2? " + (nome1 == nome2));
+  }
+}
